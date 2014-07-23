b7ab44e485bb16e81a1827308106eba9299edf8a
joemarus <joe.maruschek@gmail.com>
2014-07-23 15:02:57 -0400

    Update web_calc.pl

--- web_calc.pl
+++ web_calc.pl
@@ -1,11 +1,18 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Scalar::Util qw(looks_like_number);
 
 print "Content-type: text/html\n\n";
 
+
+#-------------------------------------------------------------------------------
+# This is Patrick's code for splitting apart the query string.
+# The results go into a hash called %in.
+#-------------------------------------------------------------------------------
 my $query = $ENV{'QUERY_STRING'};
 my %in;
+my $error = "";
 
 if (length ($query) > 0){
     my @things = split(/&/, $query);
@@ -16,10 +23,14 @@ if (length ($query) > 0){
     }
 }
 
+#-------------------------------------------------------------------------------
+# In this section, I take any passed in values and get ready to populate
+# the form fields with them.
+#-------------------------------------------------------------------------------
 my $value1 = "";
 my $value2 = "";
-if ($in{arg1}) { $value1 = $in{arg1}; }
-if ($in{arg2}) { $value2 = $in{arg2}; }
+if (length($in{arg1}) > 0) { $value1 = $in{arg1}; }
+if (length($in{arg2}) > 0) { $value2 = $in{arg2}; }
 
 my $selected1 = "";
 my $selected2 = "";
@@ -30,6 +41,9 @@ if ($in{operation} eq "subtract") { $selected2 = "SELECTED"; }
 if ($in{operation} eq "multiply") { $selected3 = "SELECTED"; }
 if ($in{operation} eq "divide") { $selected4 = "SELECTED"; }
 
+#-------------------------------------------------------------------------------
+# This is the HTML form.
+#-------------------------------------------------------------------------------
 print "<HTML><HEAD><TITLE>Simple Calculator</TITLE></HEAD>\n";
 print "<BODY><H1>A Simple Calculator</H1>\n";
 
@@ -44,15 +58,36 @@ print "</SELECT>\n";
 print "<INPUT type=\"TEXT\" size=\"10\" name=\"arg2\" value=\"$value2\">\n";
 print "<P>\n";
 print '<INPUT type="SUBMIT" value="Calculate">';
-print "</FORM>\n";
-print "<HR />\n";
+print "</FORM><HR />\n";
 print "Answer: ";
-if (($in{arg1}) && ($in{arg2})) {
+
+#-------------------------------------------------------------------------------
+# In this part, I do some checking to see if we have valid values to work with.
+#-------------------------------------------------------------------------------
+if ( (length($in{arg1}) > 0) && !looks_like_number($in{arg1}) ) {
+    $error = $error . "Your first value does not look like a number. ";
+}
+if ( (length($in{arg2}) > 0) && !looks_like_number($in{arg2}) ) {
+    $error = $error . "Your second value does not look like a number. ";
+}
+if ( ($in{arg2} eq "0") && ($in{operation} eq "divide") ) {
+    $error = $error . "Division by zero not allowed.  ";
+}
+
+#-------------------------------------------------------------------------------
+# Here is where all the calculating work is done.  If there are no errors, and
+# if there is actual data to work with, we do the calculation and print the 
+# result.  Otherwise, we print the errors that were found.
+#-------------------------------------------------------------------------------
+if ( (length($error) == 0) && (length($in{arg1}) > 0) && (length($in{arg2}) > 0) ) {
     print $in{arg1} + $in{arg2} if ($in{operation} eq "add");
     print $in{arg1} - $in{arg2} if ($in{operation} eq "subtract");
     print $in{arg1} * $in{arg2} if ($in{operation} eq "multiply");
     print $in{arg1} / $in{arg2} if ($in{operation} eq "divide");
+} else {
+    print "<SPAN style=\"color: red;\">$error</SPAN>";
 }
 
 print "</BODY></HTML>\n";
+
 
